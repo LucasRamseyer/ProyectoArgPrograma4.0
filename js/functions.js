bdba53ecdf48896fc363fec4390a50c1d6b1c927
Lucas Ramseyer <lucasramseyer@hotmail.com>
2023-02-01 22:25:37 -0300

    Primer escritura

---
+++ js/functions.js
@@ -0,0 +1,4 @@
+document.getElementById('verMas').addEventListener('click', function() {document.getElementById('textButton').style.display = "inline-block"
+})
+document.getElementById('esconder').addEventListener('click', function() {document.getElementById('textButton').style.display = "none"
+})
